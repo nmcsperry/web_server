302e124fd524e97cb85aa274598956c797e01b67
nsperry <sperrynick@icloud.com>
2025-02-09 15:04:24 -0600

    update scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,10 @@
-@mkdir build
-@pushd build
+@for %%I in (.) do @set CurrentDirectory=%%~nI%%~xI
+@setlocal
+@if "%CurrentDirectory%" == "scripts" @cd ..
+@if "%CurrentDirectory%" == "run" @cd ..
+@if not "%CurrentDirectory%" == "build" @if not exist "build" mkdir build
+@if not "%CurrentDirectory%" == "build" @cd build
+
 cl /ZI /Fe:web_server.exe ..\source\start.c user32.lib gdi32.lib opengl32.lib Ws2_32.lib
-@popd
+
+@endlocal
--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,10 @@
-@pushd run
+@for %%I in (.) do @set CurrentDirectory=%%~nI%%~xI
+@setlocal
+@if "%CurrentDirectory%" == "scripts" @cd ..
+@if "%CurrentDirectory%" == "build" @cd ..
+@if not "%CurrentDirectory%" == "run" @if not exist "run" mkdir run
+@if not "%CurrentDirectory%" == "run" @cd run
+
 ..\build\web_server.exe
-@popd
+
+@endlocal
